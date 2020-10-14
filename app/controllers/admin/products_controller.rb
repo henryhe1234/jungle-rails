0e20c602aa9a3ef814ce741b8bf3080cc1d7f14d
henryhe1234 <henryhe1123@gmail.com>
2020-10-14 23:13:32 +0000

    http authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,10 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_NAME'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
+#   HTTP_BASIC_AUTH_NAME=your_name_here
+# HTTP_BASIC_AUTH_PASSWORD=your_pass_word_here
 
   def index
+
     @products = Product.order(id: :desc).all
   end
 
