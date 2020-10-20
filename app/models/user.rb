43a81f3017d4be1e84621a96495a7f37eeecace5
henryhe1234 <henryhe1123@gmail.com>
2020-10-20 00:28:14 +0000

    create authenticate_with_credentials method and its test case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :password, length: {minimum: 5}
+
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password,presence:true
+
+  validates :password_confirmation,presence:true
+
+  def self.authenticate_with_credentials(email,password)
+    email_no_space = email.strip;
+    user =  User.where("lower(email) = ?",email_no_space.downcase).first
+    if user && user.authenticate(password)
+      # user.authenticate(password)
+      return user
+    else 
+      return nil
+    end
+    
+  end
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -19,10 +19,14 @@ RSpec.describe Product, type: :model do
     it 'validate the category field to present' do
       expect(@product.category).to be_present
     end
-    # it 'validate the name field to nil' do
-    #   expect(@product.name).to be_nil
-    #   expect(@product.errors.full_messages).to eq['Failure/Error: expect(@product.name).to be_nil']
-    # end
+    
+    it 'validate the price field to nil' do
+      @category1 = Category.new(:name => "anime")
+      @product1 = Product.new(:name => "Demon Slayer: Kimetsu no Yaiba",:description =>"best in the world",:category => @category,:quantity => 4,:image =>"https://images2.alphacoders.com/100/thumb-1920-1007550.jpg")
+      @product1.save
+      expect(@product1).to_not be_valid
+      expect(@product1.errors.full_messages).to eq ["Price cents is not a number", "Price is not a number", "Price can't be blank"]
+    end
 
   end
 end
